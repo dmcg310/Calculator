687c9428c981df3be8aeee990b6b8367d3c12ba7
dmcg310 <120114728+dmcg310@users.noreply.github.com>
2023-01-04 23:50:18 +0000

    Added basic Math functions

---
+++ script.js
@@ -0,0 +1,27 @@
+const x = parseInt(prompt("X: "));
+const y = parseInt(prompt("Y: "));
+
+function add() {
+  const total = x + y;
+  console.log(total);
+}
+
+function subtract() {
+  const total = x - y;
+  console.log(total);
+}
+
+function multiply() {
+  const total = x * y;
+  console.log(total);
+}
+
+function divide() {
+  const total = x / y;
+  console.log(total);
+}
+
+add();
+subtract();
+multiply();
+divide();
