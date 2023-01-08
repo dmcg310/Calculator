52c31e27028b48562bba062388d4f43990f94065
dmcg310 <120114728+dmcg310@users.noreply.github.com>
2023-01-08 23:28:46 +0000

    Fixed equals sign staying in the display after calculation + text overflow bug

--- script.js
+++ script.js
@@ -3,47 +3,55 @@ let displayVariable = "";
 const display = document.querySelector("#display");
 
 const buttons2 = document.querySelector(".button-row2");
-const button3 = document.querySelector(".button-row3");
-const button4 = document.querySelector(".button-row4");
-const button5 = document.querySelector(".button-row5");
-
-const clearButton = document.querySelector("#clear");
-const equalsButton = document.querySelector("#equals");
-const backspaceButton = document.querySelector("#backspace");
-
 buttons2.addEventListener("click", (e) => {
   display.textContent += e.target.textContent;
   displayVariable += e.target.textContent;
 });
 
+const button3 = document.querySelector(".button-row3");
 button3.addEventListener("click", (e) => {
   display.textContent += e.target.textContent;
   displayVariable += e.target.textContent;
 });
 
+const button4 = document.querySelector(".button-row4");
 button4.addEventListener("click", (e) => {
   display.textContent += e.target.textContent;
   displayVariable += e.target.textContent;
 });
 
-button5.addEventListener("click", (e) => {
+const decimalButton = document.querySelector("#decimal");
+decimalButton.addEventListener("click", (e) => {
   display.textContent += e.target.textContent;
   displayVariable += e.target.textContent;
 });
 
+const zeroButton = document.querySelector("#zero");
+zeroButton.addEventListener("click", (e) => {
+  display.textContent += e.target.textContent;
+  displayVariable += e.target.textContent;
+});
+const additionButton = document.querySelector("#addition");
+additionButton.addEventListener("click", (e) => {
+  display.textContent += e.target.textContent;
+  displayVariable += e.target.textContent;
+});
+
+const clearButton = document.querySelector("#clear");
 clearButton.addEventListener("click", (e) => {
   display.textContent = "";
   displayVariable = "";
 });
 
+const equalsButton = document.querySelector("#equals");
+equalsButton.addEventListener("click", (e) => {
+  operate();
+});
+
+const backspaceButton = document.querySelector("#backspace");
 backspaceButton.addEventListener("click", (e) => {
   display.textContent = display.textContent.slice(0, -1);
   displayVariable = displayVariable.slice(0, -1);
-});
-
-equalsButton.addEventListener("click", (e) => {
-  display.textContent = "";
-  operate();
 });
 
 function operate() {
